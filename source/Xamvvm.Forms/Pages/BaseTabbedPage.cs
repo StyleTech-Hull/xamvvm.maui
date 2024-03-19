84480fe72124ff15faf200b993ef8ee4dd7d6038
Adam Knight <adam.knight@styletech.co.uk>
2024-03-19 17:51:18 +0000

    Removed Xamarin Forms implementation

--- source/Xamvvm.Forms/Pages/BaseTabbedPage.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using Xamarin.Forms;
-
-namespace Xamvvm
-{
-	public class BaseTabbedPage<TPageModel> : TabbedPage, IBasePage<TPageModel> where TPageModel : class, IBasePageModel
-	{
-	}
-}
